944229fe99f38678363cc1e4fff51aa67e799b46
ian <ian@mlion.leviathan>
2014-03-04 18:42:15 +0000

    areabar improvements

--- areabar.m
+++ areabar.m
@@ -20,7 +20,7 @@ else
 end
 
 if nargin <4 || isempty(c1)
-	c1=[0.7 0.7 0.7];
+	c1=[0.5 0.5 0.5];
 end
 
 if nargin < 5 || isempty(alpha) || ischar(alpha)
@@ -28,7 +28,7 @@ if nargin < 5 || isempty(alpha) || ischar(alpha)
 		[varargin{2:end+1}]=varargin{1:end};
 		varargin{1} = alpha;
 	end
-	alpha = 1;
+	alpha = 0.5;
 end
 
 if nargin < 6 && isempty(varargin)
@@ -41,15 +41,12 @@ x=size(xvalues);
 y=size(ydata);
 e=size(error);
 
-if x(1) < x(2)             %need to organise to row
-   xvalues=xvalues';
-end
-if y(1) < y(2)             %need to organise to row
-   ydata=ydata';
-end
-if e(1) < e(2)             %need to organise to row
-   error=error';
-end
+%need to organise to rows
+if x(1) < x(2); xvalues=xvalues'; end
+if y(1) < y(2); ydata=ydata'; end
+if e(1) < e(2); error=error'; end
+
+error(isnan(error)) = 0;
 
 x=length(xvalues);
 if size(error,2) == 2
@@ -65,12 +62,14 @@ areax=zeros(x+x,1);
 areax(1:x,1)=xvalues;
 areax(x+1:x+x,1)=flipud(xvalues);
 axis auto
+if max(c1) > 1; c1 = c1 / max(c1); end
 handles.fill = fill(areax,err,c1,'EdgeColor','none','FaceAlpha',alpha);
 set(gca,'NextPlot','add');
 handles.plot = plot(xvalues,ydata,varargin{:});
+set(get(get(handles.plot,'Annotation'),'LegendInformation'),'IconDisplayStyle','off'); % Exclude line from legend
 set(gca,'NextPlot',NextPlot);
 %set(gca,'PlotBoxAspectRatioMode','manual');
 uistack(handles.plot,'top')
 set(gca,'Layer','top');
-set(gcf,'Renderer','painters');
+if alpha == 1; set(gcf,'Renderer','painters'); end
 box on;
